4ed9ab54ae869d0783958897092657dfe30cd461
awesomekimn <awesomekimn@gmail.com>
2022-08-03 19:54:10 +0900

    ff

--- src/main/java/com/spring/boardweb/controller/store/StoreController.java
+++ src/main/java/com/spring/boardweb/controller/store/StoreController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,13 +21,10 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.spring.boardweb.commons.PicUtils;
-import com.spring.boardweb.dto.StoreDTO;
-import com.spring.boardweb.dto.StoreFileDTO;
+import com.spring.boardweb.entity.CustomUserDetails;
 import com.spring.boardweb.entity.Store;
 import com.spring.boardweb.entity.StoreFile;
-import com.spring.boardweb.entity.User;
 import com.spring.boardweb.service.store.StoreService;
-import com.spring.boardweb.service.user.UserService;
 
 @RestController
 @RequestMapping("/store")
@@ -58,21 +56,19 @@ public class StoreController {
 	}
 
 	@GetMapping("/storeDetail/{storeSeq}")
-	public ModelAndView getStoreView(@PathVariable int storeSeq, HttpSession session) {
+	public ModelAndView getStoreView(@PathVariable int storeSeq, @AuthenticationPrincipal CustomUserDetails loginUser) {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("store/storeDetail.html");
 
 		Store store = storeService.getStore(storeSeq);
 
 		store.setFileList(storeService.getStoreFileList(store.getStoreSeq()));
-		store.setUserAni(storeService.getUserAni(session.getId()));
+		store.setUserAni(storeService.getUserAni(loginUser.getUsername()));
 //		List<Review> reviewList = storeService.getreviewList(reviewSeq);
-		System.out.println(store.getParking());
+//		System.out.println(store.getParking());
 		mv.addObject("store", store);
 //		mv.addObject("reviewList", reviewList);
 		
-		System.out.println(store.getUserAni());
-
 		return mv;
 	}
 
