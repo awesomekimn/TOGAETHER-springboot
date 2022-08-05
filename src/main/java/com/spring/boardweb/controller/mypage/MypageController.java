07d62b2a3a8d5cd7b0f6695291676eeb10239f56
awesomekimn <awesomekimn@gmail.com>
2022-08-05 10:12:21 +0900

    mypage review complete

--- src/main/java/com/spring/boardweb/controller/mypage/MypageController.java
+++ src/main/java/com/spring/boardweb/controller/mypage/MypageController.java
@@ -3,12 +3,14 @@ package com.spring.boardweb.controller.mypage;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.spring.boardweb.entity.CustomUserDetails;
 import com.spring.boardweb.entity.Review;
 import com.spring.boardweb.entity.Store;
 import com.spring.boardweb.entity.User;
@@ -29,7 +31,7 @@ public class MypageController {
 	StoreService storeService;
 	
 	@GetMapping("/{userId}")
-	public ModelAndView getMypage(@PathVariable String userId) {
+	public ModelAndView getMypage(@PathVariable String userId, @AuthenticationPrincipal CustomUserDetails loginUser) {
 		ModelAndView mv = new ModelAndView();
 		
 		User user = userService.idCheck(userId);
@@ -40,11 +42,33 @@ public class MypageController {
 			review.setFileList(storeService.getStoreFileList(review.getStore().getStoreSeq()));
 		}
 		
+		if(loginUser == null) {
+			mv.setViewName("user/login.html");
+		} else {
+			mv.setViewName("mypage/mypage.html");
+		}
 		
-		mv.setViewName("mypage/mypage.html");
-//		mv.addObject(store);
 		mv.addObject(user);
-		System.out.println(user + "////////////////////");
+		mv.addObject(reviewList);
+		
+		return mv;
+	}
+	
+	@GetMapping("/{userId}/showAllReview")
+	public ModelAndView showAllReview(@PathVariable String userId, @AuthenticationPrincipal CustomUserDetails loginUser) {
+		ModelAndView mv = new ModelAndView();
+		
+		User user = userService.idCheck(userId);
+		
+		List<Review> reviewList = mypageService.getMyReviewList(userId);
+		
+		if(loginUser == null) {
+			mv.setViewName("user/login.html");
+		} else {
+			mv.setViewName("mypage/" + userId + "/likeList.html");
+		}
+		
+		mv.addObject(user);
 		mv.addObject(reviewList);
 		
 		return mv;
